13194b08fd51c1c2b1187e5ce9666ca921eb7fd4
Morormo <morormo@gmail.com>
2020-05-18 19:43:17 +0200

    Heroku

--- server.js
+++ server.js
@@ -4,20 +4,28 @@ const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
 const seatsRoutes = require('./routes/seats.routes');
 const cors = require('cors');
+const path = require('path');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '/client/build')));
 
 app.use(cors());
 app.use('/api', testimonialsRoutes);
 app.use('/api', concertsRoutes);
 app.use('/api', seatsRoutes);
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
+
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found...' });
 })
 
 
-app.listen(8000, () => {
+app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
 });
