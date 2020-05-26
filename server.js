0e7afd5c7db0559bee6524ab6e31a64c7f09b9ae
Morormo <morormo@gmail.com>
2020-05-26 14:39:06 +0200

    Connect with mongoose

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const express = require('express');
+const socket = require('socket.io');
 const app = express();
 const cors = require('cors');
 const path = require('path');
-const socket = require('socket.io');
 
-const testimonialsRoutes = require('./routes/testimonials.routes');
-const concertsRoutes = require('./routes/concerts.routes');
-const seatsRoutes = require('./routes/seats.routes');
+// const testimonialsRoutes = require('./routes/testimionials.routes');
+// const concertsRoutes = require('./routes/concerts.routes');
+// const seatsRoutes = require('./routes/seats.routes');
+
+const mongoose = require('mongoose');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
+
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, '/client/build')));
@@ -20,18 +23,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/api', testimonialsRoutes);
-app.use('/api', concertsRoutes);
-app.use('/api', seatsRoutes);
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/client/build/index.html'));
-});
-
-app.use((req, res) => {
-  res.status(404).json({ message: 'Not found...' });
-});
-
 
 const server = app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
@@ -39,6 +30,26 @@ const server = app.listen(process.env.PORT || 8000, () => {
 
 const io = socket(server);
 
-io.on('connection', (socket) => {
-  console.log('New socket!');
+io.on('connection', () => {
+  console.log('New socket!')
 });
+
+/* app.use('/api', testimonialsRoutes);
+app.use('/api', concertsRoutes);
+app.use('/api', seatsRoutes);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+}); */
+
+app.use((req, res) => {
+  res.status(404).json({ message: 'Not found...' });
+})
+
+
+mongoose.connect('mongodb://localhost:27017/NewWaveDB', { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+
+db.once('open', () => {
+  console.log('Connected to the database');
+});
+db.on('error', err => console.log('Error ' + err));
